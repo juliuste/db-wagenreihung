d8fedb953503bf7c06e91e38fc2c0ee364ca7c30
Julius Tens <mail@juliustens.eu>
2018-12-12 17:30:28 +0100

    fix tests using db-hafas (r.i.p. db-zugradar), soothe the linter

--- test.js
+++ test.js
@@ -3,23 +3,38 @@
 const tapeWithoutPromise = require('tape')
 const addPromiseSupport = require('tape-promise').default
 const tape = addPromiseSupport(tapeWithoutPromise)
+const hafas = require('db-hafas')('db-wagenreihung-tests')
 const wagenreihung = require('.')
-const trainNumbers = require('db-train-numbers')
 
-tape('db-wagenreihung', async (t) => {
-	const numbers = await(trainNumbers(new Date(), new Date()))
-	const ice = numbers.find(x => x.train.length <= 7 && x.train.toLowerCase().indexOf('ice') === 0)
+tape('db-wagenreihung', async t => {
+	// find currently running train from db-hafas
+	const [berlinHbf] = await hafas.locations('Berlin Hbf', { results: 1, stations: true, addresses: false, poi: false })
+	t.ok(berlinHbf.type && !!berlinHbf.id, 'precondition')
+	const duration = 2 * 60
+	const departures = await hafas.departures(berlinHbf.id, {
+		when: new Date(Date.now() - duration * 60 * 1000),
+		duration
+	})
+	const iceDeparture = departures.find(d => d.line && d.line.product === 'nationalExp' && d.line.fahrtNr && d.line.name)
+	t.ok(iceDeparture, 'precondition')
+
+	const ice = await hafas.trip(iceDeparture.tripId, iceDeparture.line.name)
 	t.ok(ice, 'precondition')
-	const array = ice.train.split(' ')
-	const number = array[array.length - 1]
-	t.ok(+number, 'precondition')
-	t.ok(ice.lastDeparture, 'precondition')
 
-	const result = await wagenreihung(number, ice.lastDeparture)
+	const trainNumber = ice.line.fahrtNr
+	t.ok(trainNumber, 'precondition')
+
+	const lastStopovers = ice.stopovers.filter(s => s.departure && (+new Date(s.departure) < +new Date()))
+	const lastStopover = lastStopovers[lastStopovers.length - 1]
+	t.ok(lastStopover && lastStopover.departure, 'precondition')
+	const lastDeparture = new Date(lastStopover.departure)
+
+	// actual testing
+	const result = await wagenreihung(trainNumber, lastDeparture)
 	t.ok(result.product === 'ICE', 'product')
-	t.ok(result.trainNumber === number+'', 'trainNumber')
+	t.ok(result.trainNumber === trainNumber + '', 'trainNumber')
 	t.ok(result.serviceId.length > 5, 'serviceId')
-	t.ok(+ice.lastDeparture - +new Date(result.initializationDate) <= 12*60*60*1000, 'initializationDate')
+	t.ok(+lastDeparture - +new Date(result.initializationDate) <= 24 * 60 * 60 * 1000, 'initializationDate')
 	t.ok(Array.isArray(result.wagons), 'wagons type')
 	t.ok(result.wagons.length > 0, 'wagons length')
 	for (let wagon of result.wagons) {
@@ -27,7 +42,7 @@ tape('db-wagenreihung', async (t) => {
 		t.ok(wagon.type.length > 4, 'type')
 		t.ok(wagon.id.length > 4, 'id')
 		t.ok(wagon.status.length > 3, 'status')
-		t.ok(Number.isInteger(wagon.wagonNumber) || wagon.wagonNumber === null, 'wagonNumber')
+		t.ok(Number.isInteger(wagon.wagonNumber) || wagon.wagonNumber === null, 'wagonNumber')
 	}
 	t.ok(result.wagons.filter(x => Number.isInteger(x.wagonNumber)).length >= 3, 'wagonNumber')
 
